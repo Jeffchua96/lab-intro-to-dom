9a5ba302f77825485c6abb9153c56e31dd1b2213
Jeffchua96 <jeffersonchua@pursuit.org>
2023-07-07 02:55:47 -0400

    almost done

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-let sec= document.querySelector("section")
-sec.classList.add("featured")
+let element = document.querySelector("section");
+element.classList.add("featured");
 
-let article = document.createElement("article")
-let image = document.createElement("img")
-image.src = "./images/paul-gilmore-unsplash.jpg"
-image.alt = "Image of a mountain in front of a lake."
-article.append(image)
 
-let  title = document.createElement("h3")
-title.textContent = "Stop Planning"
+let article = document.createElement("article") 
+article.append("section.posts")
 
-let paragraph1 = document.createElement("p")
-paragraph1.textContent = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?"
+let section-posts=document.querySelector("section.posts")
+let article1 = document.querySelector("article1")
+let article2 = document.querySelector("article2")
+article2.before(article1)     
+
+
+
+
+
+
+
 
 let aside = document.createElement("aside")
 let asidePara = document.createElement("p")
 let span = document.createElement("span")
 let strong = document.createElement("strong")
-strong.textContent = "Read Time:"
+let textContent=document.main.select('.. read-time')
 asidePara.textContent = "4 Minutes"
 let hyperLink = document.createElement("a")
 hyperLink.href = "#"
@@ -26,11 +30,7 @@ hyperLink.textContent = "Read more..."
 span.append(strong)
 asidePara.append(span, hyperLink )
 aside.append(asidePara)
-article.append(title, paragraph1, aside)
+article.append(title, paragraph2, aside)
 
 
-let postsSection  = document.querySelector(".posts")
-postsSection.append(article)
 
-let firstPost = document.querySelector(".posts article")
-article.before(firstPost)
